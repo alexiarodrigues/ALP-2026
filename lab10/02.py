a3d28781959404f2ae12e2f2bbef3176941a7e8f
alexiarodrigues <alexiarodrigues209@gmail.com>
2026-06-09 14:53:11 -0300

    Create 02.py

---
+++ lab10/02.py
@@ -0,0 +1,12 @@
+for variavel in range(10):
+    print(variavel)
+#começa no número 1,pula antes do 5 e avança de 1 em 1
+for variavel in range(1,5):
+    (variavel)
+#
+for variavel in range(0,10,2):
+   print(variavel)
+#
+for variavel in range(10,0,-1):
+    print(variavel)
+#
